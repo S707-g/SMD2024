48118e96f4c32f26d28df1e5d3bbbb672bbf91ef
S707-g <kittipong.jc9@gmail.com>
2024-09-07 18:46:02 +0700

    [Update] Homepage 1.1

---
+++ thiraProject/src/components/homepage/H-compo/Blog.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <section className="flex-col h-screen relative">
+      <div className="h-1/6 flex justify-center">
+        <div className="text-7xl font-semibold text-blue-700 w-10/12">
+          Blog
+        </div>
+      </div>
+      <div className="flex h-5/6 items-center justify-center">
+        <div className="border-solid border-2 border-blue-700 w-10/12 h-5/6 flex divide-x-2 divide-blue-700">
+          <div className="w-1/3"></div>
+          <div className="w-1/3"></div>
+          <div className="w-1/3"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blog;
--- thiraProject/src/components/homepage/Homepage.jsx
+++ thiraProject/src/components/homepage/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./H-compo/Header";
 import Welcome from "./H-compo/Welcome";
+import Blog from "./H-compo/Blog";
 
 const Homepage = () => {
   return (
     <div className="flex-col min-h-screen bg-blue-100">
       <Header />
       <Welcome />
+      <Blog />
     </div>
   );
 };
