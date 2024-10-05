09a2b39289c641c8da93fca412fe6a3d4cf0360e
S707-g <kittipong.jc9@gmail.com>
2024-10-05 21:08:29 +0700

    Feed update

--- thiraProject/src/components/layout/NavB.jsx
+++ thiraProject/src/components/layout/NavB.jsx
@@ -3,36 +3,43 @@ import SearchIcon from "@mui/icons-material/Search";
 import { Button } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AddIcon from "@mui/icons-material/Add";
-import NotificationsNoneRoundedIcon from '@mui/icons-material/NotificationsNoneRounded';
-import Logo from '/src/assets/Thira.svg';
+import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneRounded";
+import Logo from "/src/assets/Thira.svg";
 
 const NavB = () => {
   return (
-    <div className="p-3 flex justify-between bg-black ">
-      <div className="flex justify-center text white"><img src={Logo} alt="Thira" className="h-12 w-auto" /></div>
-      
+    <div className="p-3 flex justify-between bg-gray-950 ">
+      <div className="flex justify-center text white">
+        <img src={Logo} alt="Thira" className="h-12 w-auto" />
+      </div>
+
       <div className="relative">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <SearchIcon className="text-white" />
         </div>
-        <input type="search" id="search" placeholder="Search"
-          className="bg-[#2A3236] pl-20 px-10 py-2 rounded-full text-white focus:outline-none" />
+        <input
+          type="search"
+          id="search"
+          placeholder="Search"
+          className="bg-[#2A3236] pl-20 px-10 py-2 rounded-full text-white focus:outline-none"
+        />
       </div>
 
       <div className="relative">
-        <Button variant="text" className="!text-white !capitalize !py-2 !px-4 !rounded-full">
+        <Button
+          variant="text"
+          className="!text-white !capitalize !py-2 !px-4 !rounded-full"
+        >
           <AddIcon /> Create
         </Button>
-        
+
         <Button variant="text" className="!text-white !rounded-full">
-        <NotificationsNoneRoundedIcon />
+          <NotificationsNoneRoundedIcon />
         </Button>
 
         <Button variant="text" className="!text-white !rounded-full">
           <AccountCircleIcon />
         </Button>
-
-        
       </div>
     </div>
   );
