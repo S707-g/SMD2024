4fb41305cb89637bbd5d024474f0f03eeea43886
S707-g <kittipong.jc9@gmail.com>
2024-10-09 22:50:12 +0700

    [Update] style Profile

--- thiraProject/src/components/profilePage/Profile.jsx
+++ thiraProject/src/components/profilePage/Profile.jsx
@@ -1,56 +1,74 @@
 import React, { useEffect, useState } from "react";
+import { Avatar, Button, Typography } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
 import useUser from "../../hooks/useUser";
 
 // Profile Component
 const Profile = () => {
+  const { username } = useParams();
+  const { getUserByUsername } = useUser();
+  const [bio, setBio] = useState();
 
-  const {username}= useParams()
-
-  const {getUserByUsername} = useUser()
-
-  const [bio, setBio] = useState()
-
-  useEffect(()=>{
-    getUserByUsername(username)
-    .then(data => setBio(data))
-  },[])
+  useEffect(() => {
+    getUserByUsername(username).then((data) => setBio(data));
+  }, [username, getUserByUsername]);
 
   return (
-    <div className="w-full h-full bg-gray-400">
+    <div className="w-full h-full bg-gray-400 p-3">
       {/* Header Section */}
-      <div className="bg-white h-auto sm:h-[200px] w-full p-5 sm:p-0">
-        <div className="flex flex-col sm:flex-row items-center sm:items-start">
+      <div className="bg-white p-5 rounded-lg flex-col ">
+        <div className="flex flex-col sm:flex-row items-center my-5">
           {/* Profile Picture */}
-          <img
-            src="src/components/img/blank-profile-picture-973460_1280.webp"
-            className="h-[90px] w-[90px] sm:w-auto rounded-full mt-[20px] sm:ml-[80px]"
-            alt=""
-          />
+          <div className="px-3">
+            <Avatar
+              src="src/components/img/blank-profile-picture-973460_1280.webp"
+              alt=""
+              sx={{ width: 130, height: 130 }}
+              className="mt-2 sm:mt-0 mx-auto sm:mx-0"
+            />
+          </div>
+
           {/* Name and Username */}
-          <div className="sm:ml-[20px] text-center sm:text-left mt-[20px] sm:mt-[30px]">
-            <div className="text-[20px]">{username}</div>
-            <div className="text-[10px] mt-[5px] sm:mt-[10px]">password: {bio?.password}</div>
+          <div className="mt-2 sm:mt-0 sm:ml-3 text-center sm:text-left">
+            <Typography variant="h5">{username}</Typography>
+            <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
+              Password: {bio?.password}
+            </Typography>
           </div>
         </div>
 
         {/* Navigation Menu */}
-        <div className="p-5 flex flex-row mt-[25px] mb-0 bg-gray-300">
-          <ul className="flex flex-col sm:flex-row ml-[8%] mr-[8%] text-white justify-between w-full">
-            <li className="cursor-pointer h-[40px] w-full sm:w-[90px] mb-2 sm:mb-0 rounded-[25px] flex items-center justify-center text-center bg-gray-500 hover:bg-white hover:text-gray-700">
-              <a href="#">Like</a>
-            </li>
-            <li className="cursor-pointer h-[40px] w-full sm:w-[90px] mb-2 sm:mb-0 rounded-[25px] flex items-center justify-center text-center bg-gray-500 hover:bg-white hover:text-gray-700">
-              <a href="#">Post</a>
-            </li>
-            <li className="cursor-pointer h-[40px] w-full sm:w-[90px] mb-2 sm:mb-0 rounded-[25px] flex items-center justify-center text-center bg-gray-500 hover:bg-white hover:text-gray-700">
-              <a href="#">Follower</a>
-            </li>
-            <li className="cursor-pointer h-[40px] w-full sm:w-[90px] rounded-[25px] flex items-center justify-center text-center bg-gray-500 hover:bg-white hover:text-gray-700">
-              <a href="#"> <SettingsIcon />  Setting</a>
-            </li>
-          </ul>
+        <div className="flex justify-start p-2 mt-8 rounded-md gap-6">
+          <Button
+            variant="contained"
+            color="inherit"
+            className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !hover:bg-white !hover:text-gray-700"
+          >
+            Like
+          </Button>
+          <Button
+            variant="contained"
+            color="inherit"
+            className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !hover:bg-white !hover:text-gray-700"
+          >
+            Post
+          </Button>
+          <Button
+            variant="contained"
+            color="inherit"
+            className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !hover:bg-white !hover:text-gray-700"
+          >
+            Follower
+          </Button>
+          <Button
+            variant="contained"
+            color="inherit"
+            startIcon={<SettingsIcon />}
+            className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !flex !items-center !hover:bg-white !hover:text-gray-700"
+          >
+            Settings
+          </Button>
         </div>
       </div>
     </div>
