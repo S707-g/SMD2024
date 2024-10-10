3fa9901cbdf5fcdbfd0a960cf11241700b641559
42 Siraphob <sampao.sld@gmail.com>
2024-10-10 13:45:57 +0700

    profile responsive

--- thiraProject/src/components/profilePage/Profile.jsx
+++ thiraProject/src/components/profilePage/Profile.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, Button, Typography } from "@mui/material";
+import { Avatar, Button, Typography, Grid } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { useParams } from "react-router-dom";
 import useUser from "../../hooks/useUser";
 
-// Profile Component
 const Profile = () => {
   const { username } = useParams();
   const { getUserByUsername } = useUser();
@@ -18,58 +17,68 @@ const Profile = () => {
     <div className="w-full h-full bg-gray-400 p-3">
       {/* Header Section */}
       <div className="bg-white p-5 rounded-lg flex-col ">
-        <div className="flex flex-col sm:flex-row items-center my-5">
+        <Grid container spacing={3} alignItems="center" className="my-5">
           {/* Profile Picture */}
-          <div className="px-3">
+          <Grid item xs={12} sm={4} md={3}>
             <Avatar
               src="src/components/img/blank-profile-picture-973460_1280.webp"
               alt=""
-              sx={{ width: 130, height: 130 }}
+              sx={{ width: { xs: 100, sm: 130 }, height: { xs: 100, sm: 130 } }}
               className="mt-2 sm:mt-0 mx-auto sm:mx-0"
             />
-          </div>
+          </Grid>
 
           {/* Name and Username */}
-          <div className="mt-2 sm:mt-0 sm:ml-3 text-center sm:text-left">
-            <Typography variant="h5">{username}</Typography>
+          <Grid item xs={12} sm={8} md={9} className="text-center sm:text-left">
+            <Typography variant="h5" className="break-words">
+              {username}
+            </Typography>
             <Typography variant="body2" color="text.secondary" sx={{ mt: 1 }}>
               Password: {bio?.password}
             </Typography>
-          </div>
-        </div>
+          </Grid>
+        </Grid>
 
         {/* Navigation Menu */}
-        <div className="flex justify-start p-2 mt-8 rounded-md gap-6">
-          <Button
-            variant="contained"
-            color="inherit"
-            className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !hover:bg-white !hover:text-gray-700"
-          >
-            Like
-          </Button>
-          <Button
-            variant="contained"
-            color="inherit"
-            className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !hover:bg-white !hover:text-gray-700"
-          >
-            Post
-          </Button>
-          <Button
-            variant="contained"
-            color="inherit"
-            className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !hover:bg-white !hover:text-gray-700"
-          >
-            Follower
-          </Button>
-          <Button
-            variant="contained"
-            color="inherit"
-            startIcon={<SettingsIcon />}
-            className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !flex !items-center !hover:bg-white !hover:text-gray-700"
-          >
-            Settings
-          </Button>
-        </div>
+        <Grid container spacing={2} justifyContent="" className="mt-8">
+          <Grid item>
+            <Button
+              variant="contained"
+              color="inherit"
+              className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !hover:bg-white !hover:text-gray-700"
+            >
+              Like
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="inherit"
+              className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !hover:bg-white !hover:text-gray-700"
+            >
+              Post
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="inherit"
+              className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !hover:bg-white !hover:text-gray-700"
+            >
+              Follower
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="inherit"
+              startIcon={<SettingsIcon />}
+              className="!bg-gray-500 !text-white !rounded-full !py-2 !px-4 !flex !items-center !hover:bg-white !hover:text-gray-700"
+            >
+              Settings
+            </Button>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
