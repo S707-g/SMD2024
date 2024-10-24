720886e3968070d58394ed8830612d45fa4d8e05
iamaices <premchit.ko@gmail.com>
2024-10-24 22:57:04 +0700

    ice added bar route

--- thiraProject/src/App.jsx
+++ thiraProject/src/App.jsx
@@ -5,7 +5,8 @@ import Feed from "./components/feedpage/Feed.jsx";
 import { AppointmentProvider } from "./components/feedpage/F-components/feedContent/stupidContext.jsx";
 import Layout from "./components/layout/Layout.jsx";
 import Profile from "./components/profilePage/Profile.jsx";
-
+// import BookmarkPage from './pages/BookmarkPage';
+// import FriendsPage from './pages/FriendsPage';
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route path="/feed" element={<Feed />} />
             <Route path="/profile" element={<Profile />} />
+            {/* <Route path="/bookmarks" element={<BookmarkPage />} /> */}
+            {/* <Route path="/friends" element={<FriendsPage />} /> */}
           </Route>
         </Routes>
       </Router>
--- thiraProject/src/components/feedpage/F-components/SideBarLeft.jsx
+++ thiraProject/src/components/feedpage/F-components/SideBarLeft.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom"; // Import Link for routing
 import HomeIcon from "@mui/icons-material/Home";
 import PeopleIcon from "@mui/icons-material/People";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import SendIcon from "@mui/icons-material/Send";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
-// Reusable NavButton component
-const NavButton = ({ Icon, label }) => (
+// Reusable NavButton component with Link
+const NavButton = ({ Icon, label, to }) => (
   <div className="relative p-2">
-    <Button
-      variant="text"
-      className="!text-white !capitalize !w-full !gap-4 !justify-start"
-    >
-      <Icon />
-      <span>{label}</span>
-    </Button>
+    <Link to={to} className="w-full">
+      <Button
+        variant="text"
+        className="!text-white !capitalize !w-full !gap-4 !justify-start"
+      >
+        <Icon />
+        <span>{label}</span>
+      </Button>
+    </Link>
   </div>
 );
 
@@ -24,15 +27,15 @@ const SideBarLeft = () => {
     <div className="w-[250px] bg-black flex flex-col sticky top-0">
       {/* Top Section */}
       <div>
-        <NavButton Icon={HomeIcon} label="Home" />
-        <NavButton Icon={PeopleIcon} label="Friends" />
-        <NavButton Icon={BookmarkIcon} label="Bookmarks" />
-        <NavButton Icon={SendIcon} label="Post" />
+        <NavButton Icon={HomeIcon} label="Home" to="/" />
+        <NavButton Icon={PeopleIcon} label="Friends" to="/friends" />
+        <NavButton Icon={BookmarkIcon} label="Bookmarks" to="/bookmarks" />
+        <NavButton Icon={SendIcon} label="Post" to="/post" />
       </div>
 
       {/* Logout Button Positioned at the Bottom */}
       <div className="mt-auto">
-        <NavButton Icon={ExitToAppIcon} label="Logout" />
+        <NavButton Icon={ExitToAppIcon} label="Logout" to="/login" />
       </div>
     </div>
   );
